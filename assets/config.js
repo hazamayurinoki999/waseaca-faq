9e81c77b8c9e447041ecda186d56d59b4452e42e
hazamayurinoki999 <mizobata.y@waseaca.com>
2025-11-06 22:22:43 +0900

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -3,5 +3,5 @@ window.CONFIG = {
   SHEET_NAME: "FAQ",
   HP_LINK: "https://waseaca-singapore.com/",
   REQUIRED_HEADERS: ["カテゴリ","質問","回答","公開フラグ"],
-  APPS_SCRIPT_ENDPOINT: "<ConfigシートのURLをここに貼るか、後で上書き>",
+  APPS_SCRIPT_ENDPOINT: "https://script.google.com/macros/s/AKfycbyfNdiqxZ-KAPHcJ_xyJSQS9woMT7am7daGjMC62X0o9EiXx8VZGjQQ2KragJVoWQ/exec",
 };
